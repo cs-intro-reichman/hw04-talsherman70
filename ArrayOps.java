bae01f69825c626b8e3a99601b8496f170e9cc3f
talsherman70 <tal.sherman@post.runi.ac.il>
2024-01-16 09:12:34 +0200

    Add my code

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,97 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int[] arr1 = {10 ,9 ,8};
+        int[] arr2 = {10, 9 , 8 ,8 ,9};
+        System.out.println(containsTheSameElements(arr1, arr2));
     }
     
+    //Gets an array with the length n, that holds all the integrs from 0 to n except one, and then rturns the missing integer
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int[] array2 = new int[array.length + 1];
+        int ans = 0;
+        for (int i = 0; i < array.length; i++){
+            array2[array[i]]++;
+        }
+        for (int i = 0; i < array2.length; i++){ 
+            if(array2[i] == 0){
+                ans = i;
+            }
+        }       
+        return ans;
     }
-
+    //Gets an array of integers and rturns the integer with the seconed highest value
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max = Math.max(array[0], array[1]);
+        int seconedMax = Math.min(array[0], array[1]);
+
+        for (int i = 2; i < array.length; i++){
+            if (array[i] > seconedMax){
+                if(array[i] > max){
+                    seconedMax = max;
+                    max = array[i];
+                }
+                else{
+                    seconedMax = array[i];
+                }
+            }
+        }
+        return seconedMax;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+    public static boolean containsTheSameElements(int [] array1, int [] array2) {
+        boolean ans = true;
+        int count = 0;
+        for(int i = 0; i < array1.length; i++){
+            for (int j = 0; j < array2.length; j++){
+                if (array1[i]!= array2[j]){
+                    count++;
+                }
+            }
+            if (count ==array2.length){
+                ans = false;
+                break;
+            }
+            else{
+                count = 0;
+            }
+        }
+        
+        
+        return ans;
     }
 
+    //Gets an array of integers and returns whether the array is sorted or not
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        int i = 1;
+        boolean ans = true;
+        boolean isDecreasing = false;
+        boolean isIncreasing = false;
+
+        while ((isDecreasing == false && isIncreasing == false) && array.length > i) {
+             if (array[i] < array [i-1]){
+                isDecreasing = true;
+             }  
+              if (array[i] > array [i-1]){
+                isIncreasing = true;
+             }   
+             i++;
+        } 
+        i = 1;
+        while (i < array.length && ans) {
+            if (isIncreasing){
+                if (array[i] < array[i-1]){
+                    ans = false;
+                }
+            }
+            
+            if (isDecreasing){
+                if (array[i] > array[i-1]){
+                    ans = false;
+                }
+            }
+            i++;
+        }
+        return ans;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,97 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        //System.out.println(camelCase("hello woRld aNd Hello"));
+        int[] arr = allIndexOf("superduper ter",'r');
+        System.out.println(camelCase("   Intro to coMPUter sCIEncE"));
+    
     }
 
+    //Gets a string and turns all the vowels into capital letters, and all the rest to lowcase.
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String change =""; 
+        for (int i = 0; i < string.length(); i++){
+            char c = string.charAt(i);
+            if(c == 'a' ||c == 'e' ||c == 'i' ||c == 'o' ||c == 'u'){
+                c = (char) (c - 'a' + 'A');
+            }
+            else if ('A' <= c && c <='Z') {
+                    c = (char) (c - 'A' + 'a');
+            }
+
+            change += c;
+        }
+        return change;
     }
 
+    //Gets a string, turns all the first letters of each word except the first one to capital, lowcases the rest and removes all spaces between words
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String change= "";
+        string = lowerCase(string);
+        for (int i = 0; i < string.length(); i++){
+            char c = string.charAt(i);
+            if(i >1){
+            if(string.charAt(i-1) == ' ' && 'a' <= c && c <='z' ){
+                c = (char) (c - 'a' + 'A');
+
+            }
+        }
+        change += c;
+        change = removeSpaces(change);  
+        //change = lowerFirstChar(change);
+
+        }
+        return change;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int[] array = new int[string.length()];
+        int j = 0;
+        for (int i = 0; i < string.length(); i++){
+            if(chr == string.charAt(i)){
+                array[j] = i;
+                j++;
+            }
+        }
+        return array;
+    }
+
+    //Helper function- turns all the letters in a string to lowcase
+   public static String lowerCase(String str) {
+        String change = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if ('A' <= c && c <='Z') {
+                c = (char) (c - 'A' + 'a');
+            }
+            change += c;
+        }
+        return change;
+    }
+
+    //Helper function- removes all the space characters in a string
+    public static String removeSpaces (String string){
+        String change = "";
+        for (int i = 0; i < string.length(); i++){
+            char c = string.charAt(i);
+            if(c != ' '){
+                change += c;
+            }
+        }
+        return change;
+    }
+
+    //Helper function- turns the first letter in a string to Lowcase
+    public static String lowerFirstChar(String string){
+        String change = "";
+        char c =string.charAt(0);
+
+        if ('A' <= c && c <='Z'){
+            c = (char) (c - 'A' + 'a');
+        }
+        change +=c;
+        change += string.substring(1, string.length());
+        return change;
     }
 }
